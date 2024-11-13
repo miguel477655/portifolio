4ba13c294ed643e51c3d67acf1bec5c3d5b34f6f
miguel477655 <167791415+miguel477655@users.noreply.github.com>
2024-11-13 09:44:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para abrir e fechar a aba lateral
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '-250px') {
+        sidebar.style.left = '0';
+    } else {
+        sidebar.style.left = '-250px';
+    }
+}
